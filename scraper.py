a4b6cc0ac8d56dfb23cbaeae09828657f7b67554
Rongbing Lai <rongbil1@uci.edu>
2022-10-27 22:32:21 -0700

    installed dependencies

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import re
 from urllib.parse import urlparse
 
+from bs4 import BeautifulSoup
+# soup = BeautifulSoup(html_doc, 'html.parser')
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -15,7 +18,9 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    result = list()
+    
+    return result
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
